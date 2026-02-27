78137db41dbc4ac256e0f4e43be7ec6c9805c395
Harsh Gaur <hgaur701@gmail.com>
2026-02-27 18:38:08 +0530

    UPDATED: added 2 more functions

--- lua/ez-multi-cursor/utils.lua
+++ lua/ez-multi-cursor/utils.lua
@@ -6,10 +6,10 @@ local M = {}
 ---@param buf integer
 ---@return string
 function M.get_line(line_number, buf)
-    local start = line_number - 1
-    local finish = line_number
-    local lines = vim.api.nvim_buf_get_lines(buf, start, finish, false)
-    return lines[1] or ""
+	local start = line_number
+	local finish = line_number + 1
+	local lines = vim.api.nvim_buf_get_lines(buf, start, finish, false)
+	return lines[1] or ""
 end
 
 --- Replace_line: Replace a specific line in the current buffer.
@@ -18,14 +18,41 @@ end
 ---@param bufnr integer        -- buffer number
 ---@return string -- Newly created line
 function M.replace_line(line_number, new_text, bufnr)
-    local start = line_number - 1
-    local finish = line_number
+	local start = line_number
+	local finish = line_number + 1
 
-    -- Replace the line
-    vim.api.nvim_buf_set_lines(bufnr, start, finish, false, { new_text })
-    local lines = vim.api.nvim_buf_get_lines(bufnr, start, finish, false)
+	-- Replace the line
+	vim.api.nvim_buf_set_lines(bufnr, start, finish, false, { new_text })
+	local lines = vim.api.nvim_buf_get_lines(bufnr, start, finish, false)
 
-    return lines[1] or ""
+	return lines[1] or ""
+end
+
+--- Returns current cursor's position (row and col)
+---@param win integer
+---@return integer
+---@return integer
+function M.get_cursor_position(win)
+	local pos = vim.api.nvim_win_get_cursor(win)
+	local row, col = pos[1], pos[2]
+
+	return row, col
+end
+
+function M.is_there_already_an_extramark(target_row, target_col, buf, namespace)
+	local existing_extramark = vim.api.nvim_buf_get_extmarks(buf, namespace, { target_row, target_col },
+		{ target_row, target_col },
+		{ details = true })
+
+	for _, mark in ipairs(existing_extramark) do
+		local id, row, col = mark[1], mark[2], mark[3]
+
+		if (row == target_row) and (col == target_col) then
+			return { exist = true, id = id }
+		end
+	end
+
+	return { exist = false, id = nil }
 end
 
 return M
